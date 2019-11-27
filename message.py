aaf6b17cdd10e2aa3da12a4cbc33a222775c6bcc
srr013 <srr013@gmail.com>
2019-11-27 08:10:54 -0500

    sad

--- message.py
+++ message.py
@@ -4,6 +4,7 @@ from urllib.request import Request, urlopen
 import random
 import logging
 import json
+import insults
 
 def ad_hoc_message(msg,id):
 	if isinstance(msg,str):
@@ -34,21 +35,25 @@ def reply_with_mention(msg, user, user_id, bot_id):
 
 def get_message(user):
 	user = "@"+user
-	lead_in = ["Bleep bloop bleep. "+user+ " is ", "Congratulations "+user+" you are ", 
-	"My calculations have revealed that "+user+" is ", "I heard that "+user+" is "]
-	messages = {
-	0 : ['idiotic', 'dumber than a sack of potatoes', 'only valuable in the sack', "a straight up moron",
-	 "... well let's just say bless your heart", "as good at Fantasy Football as Owen Reese", "the human form of a bad joke", 
-	 "reminiscent of nothing memorable", "...nevermind. Go f yaself", "as factually inaccurate as a Republican talking point",
-	 "not worth the time it took to write this. Dumbass.", "fishier than Hillary Clinton's underpants on a hot day"],
-	1 : ["smarter than expected", "amazing", "insightful and heartwarming", "attractive",
-	 "probably better than I could've done", "actually not that dumb", "a bit better than average", "a pro",
-	 "as beautiful and talented as Tom Brady", "the Gordon Ramsey of Fantasy Footballers"]
-	}
-	m = random.randint(0,1)
-	message = messages[m][random.randint(0, len(messages[m]))-1]
-	lead = lead_in[random.randint(0,len(lead_in))-1]
-	msg = lead + message
+	# lead_in = ["Bleep bloop bleep. "+user+ " is ", "Congratulations "+user+" you are ", 
+	# "My calculations have revealed that "+user+" is ", "I heard that "+user+" is "]
+	# messages = {
+	# 0 : ['idiotic', 'dumber than a sack of potatoes', 'only valuable in the sack', "a straight up moron",
+	#  "... well let's just say bless your heart", "as good at Fantasy Football as Owen Reese", "the human form of a bad joke", 
+	#  "reminiscent of nothing memorable", "...nevermind. Go f yaself", "as factually inaccurate as a Republican talking point",
+	#  "not worth the time it took to write this. Dumbass.", "fishier than Hillary Clinton's underpants on a hot day"],
+	# 1 : ["smarter than expected", "amazing", "insightful and heartwarming", "attractive",
+	#  "probably better than I could've done", "actually not that dumb", "a bit better than average", "a pro",
+	#  "as beautiful and talented as Tom Brady", "the Gordon Ramsey of Fantasy Footballers"]
+	# }
+	insult_list = insults.insults
+	m = insult_list[random.randint(0,len(insult_list)-1)]
+	if " " not in m:
+		m+= " " + insult_list[random.randint(0,len(insult_list)-1)]
+	# message = messages[m][random.randint(0, len(messages[m]))-1]
+	# lead = lead_in[random.randint(0,len(lead_in))-1]
+	# msg = lead + message
+	msg = user + "is a "+ m
 	return msg
 
 # Send a message in the groupchat
