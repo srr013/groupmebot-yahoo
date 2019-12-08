c96fe7f4d467c746ea1a2858688deea11086142c
srr013 <srr013@gmail.com>
2019-12-08 10:03:41 -0500

    sd

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def webhook():
 			if group_data['message_num'] >= group_data['message_limit'] and not m.sender_is_bot(message):
 				logging.warning("message: "+ message['text']+", "+
 					str(group_data['message_num'])+" / "+str(group_data['message_limit'])+
-					"message_full: " +str(json.dumps(message))+", Chat: "+bot_id)
+					"message_full: " +str(json.dumps(message))+", Chat: "+group_data['bot_id'])
 				groupme_bot.reset_message_data(group_data['index'])
 				m.reply_with_mention(m.get_message(message['name']),
 				message['name'], message['sender_id'], bot_id)
