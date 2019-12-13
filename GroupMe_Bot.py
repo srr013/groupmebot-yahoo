1389804e91d15d02c2bfae8fcb61f9c18026406b
srr013 <srr013@gmail.com>
2019-12-13 17:48:00 -0500

    ds

--- GroupMe_Bot.py
+++ GroupMe_Bot.py
@@ -208,7 +208,7 @@ class GroupMe_Bot():
 		select = "SELECT messages FROM groupme_yahoo WHERE groupme_group_id = %s;"
 		select_values = (message['group_id'],)
 		cursor = db.execute_table_action(select, values=select_values, cur=True)
-		messages = [m for m in json.loads(cursor.fetchall())]
+		messages = cursor.fetchall()
 		messages.insert(0,message)
 		query = "UPDATE groupme_yahoo SET messages= %s WHERE groupme_group_id = %s;"
 		values = (json.dumps(messages), str(message['group_id']))
