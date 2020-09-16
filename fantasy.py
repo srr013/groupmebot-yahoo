0ecbc7a8b618dda6a41de06cfdaba1dfdb6c4278
srr013 <srr013@gmail.com>
2020-09-15 21:11:12 -0400

    bug fix

--- fantasy.py
+++ fantasy.py
@@ -141,20 +141,19 @@ def get_transaction_list(data, past_trans_total):
 def get_scoreboard(league_data, teams):
     scoreboard = []
     matchups = league_data['scoreboard']['fantasy_content']['league'][1]['scoreboard']["0"]['matchups']
-    logging.warn(matchups)
     for m in matchups.keys():
-        logging.warn(matchups[m])
-        match = {
+        if m != 'count':
+            match = {
                 'team_0_id': matchups[m]['matchup']['0']['teams']['0']['team'][0][1]['team_id'],
                 'team_0_score': matchups[m]['matchup']['0']['teams']['0']['team'][1]['team_points']['total'],
                 'team_1_id': matchups[m]['matchup']['0']['teams']['1']['team'][0][1]['team_id'],
                 'team_1_score': matchups[m]['matchup']['0']['teams']['1']['team'][1]['team_points']['total'],
             }
-        for t in teams:
-            if t == match['team_0_id']:
-                match['team_0_owner'] = teams[t]
-            elif t == match['team_1_id']:
-                match['team_1_owner'] = teams[t]
-        scoreboard.append(match)
+            for t in teams.keys():
+                if t == match['team_0_id']:
+                    match['team_0_owner'] = teams[t]
+                elif t == match['team_1_id']:
+                    match['team_1_owner'] = teams[t]
+            scoreboard.append(match)
 
     return scoreboard
